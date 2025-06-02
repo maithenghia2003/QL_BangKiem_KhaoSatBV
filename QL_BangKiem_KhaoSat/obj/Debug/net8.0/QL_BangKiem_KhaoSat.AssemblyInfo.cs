09d4dd88e286b6df7060044dc8c18f880bb3cf45
NewTranWork <143317056+NewTranWork@users.noreply.github.com>
2025-06-02 19:03:37 +0700

    Cập nhật cấu hình 2/6

--- QL_BangKiem_KhaoSat/obj/Debug/net8.0/QL_BangKiem_KhaoSat.AssemblyInfo.cs
+++ QL_BangKiem_KhaoSat/obj/Debug/net8.0/QL_BangKiem_KhaoSat.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QL_BangKiem_KhaoSat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae603961394419cc07d8b347d0783d363980de3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac39828425ff11e2d34011f03f685f3fc1a02ce1")]
 [assembly: System.Reflection.AssemblyProductAttribute("QL_BangKiem_KhaoSat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QL_BangKiem_KhaoSat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
